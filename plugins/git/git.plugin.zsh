db112624fbbe38fe60fb7b8c023df5cb07f0e4e2
Tony Deng <wolf.deng@gmail.com>
2023-09-10 22:54:07 +0800

    去掉gm的alias

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -94,9 +94,6 @@ alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
 # Sign and verify commits with GPG
 alias gcs='git commit -S'
 
-# Sign and verify commits with GPG
-alias gcs='git commit -S'
-
 alias gd='git diff'
 alias gdca='git diff --cached'
 alias gdcw='git diff --cached --word-diff'
