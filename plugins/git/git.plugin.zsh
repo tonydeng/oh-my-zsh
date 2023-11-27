c512ab4e7133368d095838ea6f04e7d88e4c4a0a
Tony Deng <wolf.deng@gmail.com>
2023-11-27 23:03:58 +0800

    :bug: fix(1.0): 避免了mvnd和maven-plugin的别名冲突
    避免了mvnd和maven-plugin的别名冲突
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -59,16 +59,6 @@ alias gco='git checkout'
 alias gcor='git checkout --recurse-submodules'
 alias gcount='git shortlog --summary --numbered'
 alias gcp='git cherry-pick'
-<<<<<<< HEAD
-alias gcpa='git cherry-pick --abort'
-alias gcpc='git cherry-pick --continue'
-alias gcs='git commit --gpg-sign'
-alias gcss='git commit --gpg-sign --signoff'
-alias gcssm='git commit --gpg-sign --signoff --message'
-<<<<<<< HEAD
-=======
-=======
->>>>>>> aeb1ff69 (去掉gm的alias)
 compdef _git gcp=git-cherry-pick
 alias glg='git log --stat --max-count=10'
 compdef _git glg=git-log
